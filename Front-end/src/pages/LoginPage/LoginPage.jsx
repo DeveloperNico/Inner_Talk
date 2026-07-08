694f1abd5f193fb8f1b0e315407255b5531886f0
Duarte Nicolas (CaP/ETS3) <ini2ca@bosch.com>
2026-07-08 14:50:00 -0300

    Começado a página de login

--- Front-end/src/pages/LoginPage/LoginPage.jsx
+++ Front-end/src/pages/LoginPage/LoginPage.jsx
@@ -4,13 +4,17 @@ import styles from './LoginPage.module.css';
 
 export function LoginPage() {
     return (
-        <>
+        <main className={styles.mainContainer}>
+            <div>
+                <h2>Bem-vindo de volta ao InnerTalk</h2>
+            </div>
+
             <h1>Login Page</h1>
 
             <div className={styles.container}>
                 <Link to="/chatbot">Go to ChatBot Page</Link>
                 <Link to="/">Go to Home Page</Link>
             </div>
-        </>
+        </main>
     );
 }
